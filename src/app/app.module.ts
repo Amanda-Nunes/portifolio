5f15d015f04a76e85d1a110e6541cb78907a0cd4
Giovanni Sarao Araki <giovanni_s_araki@yahoo.com.br>
2020-03-28 04:53:03 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { SidemenuComponent } from './shared/components/side-menu/sidemenu/sidemenu.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { ParticlesModule } from 'angular-particle';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/home/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': 1,
+      'top': 0,
+      'left': '5%',
+      'right': 0,
+      'bottom': 0,
+  };
+
+this.myParams = {
+      particles: {
+          number: {
+              value: 50,
+          },
+          color: {
+              value: '#750FF7'
+          },
+          shape: {
+              type: 'polygon',
+          },
+  }
+};
+  }
+
+}
